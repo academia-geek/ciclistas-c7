402cd2a2b16dab6a34abf62022e11eef74831717
devagiloscarmesa <oscar.mesa@agileinnova.org>
2021-06-01 11:38:24 -0500

    cambios en funciones

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var x  = 1   
+const a = 1
+let z = 3
+
+z = x + a
+    
+console.log("El valor de z es ",  z, typeof(z))
+
+z = "hola"
+    
+console.log(typeof(z))
+
+
+function clic(valor1, valor2){
+    valor2 = 3
+    if(valor1 != valor2){
+        var PI = 3.1416
+    }
+    console.log(valor1 + valor2)
+    console.log(PI)
+    return PI
+}
+
+/*
+function sumar(x, y){
+    return x + y
+}
+
+sumar = (x, y) => {
+    return x + y
+} */
+
+sumar = (x , y ) => {
+    console.log("mensaje") 
+    const PI = 16 
+    x + y
+}
+
+miFuncion1 = () => 5 + 7
+
+if(1 < 3){
+    var nombre = "juan"        
+    console.log(nombre)
+}
+
+console.log(nombre)
