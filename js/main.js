61279db90d04d5120622c944b9522db0ce61632c
devagiloscarmesa <oscar.mesa@agileinnova.org>
2021-06-01 13:05:46 -0500

    Cambios 1 de junio

--- js/main.js
+++ js/main.js
@@ -1,46 +1,54 @@
-var x  = 1   
-const a = 1
-let z = 3
+const CORREDORES = []
+document.querySelector('#frm-ciclista').addEventListener('submit', e => {
+  let nombre = document.querySelector('nombre_ciclista').value
+  let carrera1 = document.querySelector('carrera_1_corredor').value
+  let carrera2 = document.querySelector('carrera_2_corredor').value
+  let carrera3 = document.querySelector('carrera_3_corredor').value
+  let carrera4 = document.querySelector('carrera_4_corredor').value
+  let carrera5 = document.querySelector('carrera_5_corredor').value
+  const corredor = {
+    nombre: nombre,
+    carrera1: carrera1,
+    carrera2: carrera2,
+    carrera3: carrera3,
+    carrera4: carrera4,
+    carrera5: carrera5
+  }
 
-z = x + a
-    
-console.log("El valor de z es ",  z, typeof(z))
+  CORREDORES.push(corredor)
+  e.preventDefault()
+})
 
-z = "hola"
-    
-console.log(typeof(z))
-
-
-function clic(valor1, valor2){
-    valor2 = 3
-    if(valor1 != valor2){
-        var PI = 3.1416
-    }
-    console.log(valor1 + valor2)
-    console.log(PI)
-    return PI
-}
+console.log(corredor)
+//if(CORREDORES instanceof 'Object')
+console.log(CORREDORES.length)
 
 /*
-function sumar(x, y){
-    return x + y
+for (let i = 0; i < CORREDORES.length; i++) {
+    console.log(CORREDORES[i])
+} 
+
+let i = 0
+while(i < CORREDORES.length) {
+    console.log(CORREDORES[i])
+    i =  i + 1
 }
 
-sumar = (x, y) => {
-    return x + y
-} */
-
-sumar = (x , y ) => {
-    console.log("mensaje") 
-    const PI = 16 
-    x + y
+for (i in CORREDORES) {
+  console.log(CORREDORES[i])
 }
 
-miFuncion1 = () => 5 + 7
 
-if(1 < 3){
-    var nombre = "juan"        
-    console.log(nombre)
+for(cor of CORREDORES){
+    console.log(cor)
+}*/
+
+CORREDORES.forEach(cor => console.log('El indice es : ', cor))
+
+/*
+const sumar = (x, y, z, p) => {
+    console.log("La suma es : ", (x + y + z + p))
 }
 
-console.log(nombre)
+sumar(...CORREDORES)
+*/
